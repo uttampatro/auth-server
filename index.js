0f9cc02d073dfcd76ed5564902d15a1a941ba667
uttampatro <uttampatro890@gmail.com>
2021-04-14 22:27:57 +0530

    fixed

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 //import
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const authRoute = require('./routes/auth')
-
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const cors = require("cors");
 
 //app config
 const app = express();
 const port = process.env.PORT || 5000;
 
-dotenv.config()
+dotenv.config();
 
 //Middleware
 app.use(express.json());
-
+app.use(cors())
 
 //DB config
-mongoose.connect(process.env.DB_CONNECT, {
+mongoose.connect(
+  process.env.DB_CONNECT,
+  {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-},
-    () => console.log('connected to DB')
-)
+  },
+  () => console.log("connected to DB")
+);
 
 //Api middleware
-app.use('/user', authRoute)
-
+app.use("/user", authRoute);
 
 //listener
-app.listen(port, () => console.log(`localhost running on ${port}`))
+app.listen(port, () => console.log(`localhost running on ${port}`));
--- routes/auth.js
+++ routes/auth.js
@@ -1,54 +1,57 @@
-const User = require('../entites/User');
-const router = require('express').Router()
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
+const User = require("../entites/User");
+const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-
-router.post('/register', async (req, res) => {
-
+router.post("/register", async (req, res) => {
+  try {
     //Checking if the email exists
-    const emailExists = await User.findOne({email: req.body.email})
-    if(emailExists){
-        return res.status(400).send('Email already exists')
+    const emailExists = await User.findOne({ email: req.body.email });
+    if (emailExists) {
+      return res.status(400).send("Email already exists");
     }
 
     //Hash the password
-    const salt = await bcrypt.genSalt(10)
-    const hasedPassword = await bcrypt.hash(req.body.password, salt)
+    const salt = await bcrypt.genSalt(10);
+    const hasedPassword = await bcrypt.hash(req.body.password, salt);
 
     const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: hasedPassword
-    })
-    try{
-        const savedUser = await user.save()
-        res.send(savedUser)
-    }
-    catch(err){
-        res.status(400).send(err)
-    }
-})
+      name: req.body.name,
+      email: req.body.email,
+      password: hasedPassword,
+    });
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (err) {
+    res.status(500).send(err);
+    console.log(err);
+  }
+});
 
-router.post('/login', async (req, res) => {
-
-
+router.post("/login", async (req, res) => {
+  try {
     //Checking email exists or not
-    const emailExists = await User.findOne({email: req.body.email})
-    if(!emailExists){
-        return res.status(400).send('Email not found!')
+    const emailExists = await User.findOne({ email: req.body.email });
+    if (!emailExists) {
+      return res.status(400).send("Email not found!");
     }
 
     //password is correct or not
-    const validPassword = await bcrypt.compare(req.body.password, emailExists.password)
-    if(!validPassword) {
-        return res.status(400).send('Invalid password')
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      emailExists.password
+    );
+    if (!validPassword) {
+      return res.status(400).send("Invalid password");
     }
 
     //jwt
-    const token = jwt.sign({_id: emailExists._id}, process.env.TOKEN_SECRET);
-    res.header('auth-token', token).send(token)
-
-})
+    const token = jwt.sign({ _id: emailExists._id }, process.env.TOKEN_SECRET);
+    res.header("auth-token", token).send(token);
+  } catch (err) {
+    res.status(500).send(err);
+    console.log(err);
+  }
+});
 
 module.exports = router;
